0a58cb290b46257148b06875a21a9f02f8998c31
AnibalFuentes <anibaldfuentes@unicesar.edu.co>
2022-11-30 08:47:00 -0500

    Capa logica de ganados

--- Negocio/N_Ganado.cs
+++ Negocio/N_Ganado.cs
@@ -6,7 +6,24 @@ namespace Negocio
 {
     public class N_Ganado
     {
+        D_Ganado datosGanado = new D_Ganado();
 
+        public List<Ganado> Listar()
+        {
+            return datosGanado.ListarGanados();
+        }
+        public int Registrar(Ganado Ganado, out string mensaje)
+        {
+            return datosGanado.RegistrarGanado(Ganado, out mensaje);
+        }
+        public bool Editar(Ganado Ganado, out string mensaje)
+        {
+            return datosGanado.EditarGanado(Ganado, out mensaje);
+        }
+        public bool Eliminar(Ganado Ganado, out string mensaje)
+        {
+            return datosGanado.EliminarGanado(Ganado, out mensaje);
+        }
     }
 }
 
