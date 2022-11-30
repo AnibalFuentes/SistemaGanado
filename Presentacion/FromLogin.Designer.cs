dd6daeb8fb2e025bb7e39dbff0c7d00f0730b4a1
AnibalFuentes <anibaldfuentes@unicesar.edu.co>
2022-11-30 08:55:04 -0500

    Capa de negocio y login trabajado

---
+++ Presentacion/FromLogin.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Presentacion
+{
+    partial class FromLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usuario = new System.Windows.Forms.TextBox();
+            this.contraseña = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 20F);
+            this.label1.Location = new System.Drawing.Point(64, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Iniciar sesión";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña:";
+            // 
+            // usuario
+            // 
+            this.usuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.usuario.Location = new System.Drawing.Point(70, 99);
+            this.usuario.Name = "usuario";
+            this.usuario.Size = new System.Drawing.Size(200, 26);
+            this.usuario.TabIndex = 3;
+            // 
+            // contraseña
+            // 
+            this.contraseña.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.contraseña.Location = new System.Drawing.Point(70, 154);
+            this.contraseña.Name = "contraseña";
+            this.contraseña.Size = new System.Drawing.Size(200, 26);
+            this.contraseña.TabIndex = 4;
+            this.contraseña.UseSystemPasswordChar = true;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Location = new System.Drawing.Point(70, 199);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(200, 30);
+            this.btnIngresar.TabIndex = 5;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // FromLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 261);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.contraseña);
+            this.Controls.Add(this.usuario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(350, 300);
+            this.MinimumSize = new System.Drawing.Size(350, 300);
+            this.Name = "FromLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Iniciar sesión";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usuario;
+        private System.Windows.Forms.TextBox contraseña;
+        private System.Windows.Forms.Button btnIngresar;
+    }
+}
